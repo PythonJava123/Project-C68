32ac293b5cddd6d6eaea762947ccfa01745380d5
PythonJava123 <58656406+PythonJava123@users.noreply.github.com>
2021-05-09 08:56:54 +0530

    Add files via upload

---
+++ Screens/ig.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, View, TouchableOpacity, Stylesheet} from 'react-native';
+
+export default class IGScreen extends React.Component{
+    render(){
+        return(
+            <View style={{marginTop: 150, marginLeft: 100}}>
+                <Text>Instagram</Text>
+            </View>
+        )
+    }
+}
